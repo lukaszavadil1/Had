2645d8684bf31f53e5ac126abef54e72dd88c00f
Lukas <lukaszavadil@gmail.com>
2020-10-03 12:49:58 +0200

    basic template

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-import pygame
-
-pygame.init()
-
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
-Display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-icon = pygame.image.load("imgs/logo.png")
-pygame.display.set_icon(icon)
-pygame.display.set_caption("Had")
+import pygame, sys
+from settings import *
 
 snake_1_head = pygame.image.load("imgs/snake_head.png")
 snake_2_head = pygame.image.load("imgs/snake_head_2.png")
 apple_img = pygame.image.load("imgs/apple.png")
 obstacle_img = pygame.image.load("imgs/obstacle.png")
 
-pygame.display.update()
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.running = True
+        self.icon = pygame.image.load("imgs/logo.png")
+        pygame.display.set_icon(self.icon)
+        pygame.display.set_caption("Had")
+
+    def run(self):
+        while self.running:
+            self.get_events()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
+
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and pygame.K_ESCAPE:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill(BG_COLOR)
+        pygame.display.update()
 
 
-def exit_game():
-    pygame.quit()
-    quit()
-
-
-exit_game()
---
+++ main.py
@@ -0,0 +1,5 @@
+from app import *
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
