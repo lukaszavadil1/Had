0e8140648f032238aaff43b1abc68f6f7eb15117
Lukas <lukaszavadil@gmail.com>
2020-10-03 12:58:30 +0200

    intro state

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ class App:
         self.icon = pygame.image.load("imgs/logo.png")
         pygame.display.set_icon(self.icon)
         pygame.display.set_caption("Had")
+        self.state = "intro"
 
     def run(self):
         while self.running:
@@ -23,19 +24,32 @@ class App:
             self.update()
             self.draw()
             self.clock.tick(FPS)
+        pygame.quit()
+        sys.exit()
 
     def get_events(self):
+        if self.state == "intro":
+            self.intro_events()
+
+    def update(self):
+        if self.state == "intro":
+            self.intro_update()
+
+    def draw(self):
+        self.screen.fill(BG_COLOR)
+        if self.state == "intro":
+            self.intro_draw()
+        pygame.display.update()
+
+    def intro_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             if event.type == pygame.KEYDOWN and pygame.K_ESCAPE:
                 self.running = False
 
-    def update(self):
+    def intro_update(self):
         pass
 
-    def draw(self):
-        self.screen.fill(BG_COLOR)
-        pygame.display.update()
-
-
+    def intro_draw(self):
+        pass
